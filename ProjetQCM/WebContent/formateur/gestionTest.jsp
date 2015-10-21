ecf79047c909e869c35deb8aabd5a5e9e2cef9c9
d1410jouennej <d1410jouennej@21-UC36-01.ad.campus-eni.fr>
2015-10-21 15:01:45 +0200

    - Modification TestDAO. - ajout gestionTest.jsp - ajout listeTest.jspf - ajout detailTest.jspf - Ajout Servlet GestionTest.java

---
+++ ProjetQCM/WebContent/formateur/gestionTest.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<%@ include file="/menu.jsp"%>
+	<div id="contenu">
+		<h1>Gestion des tests</h1>
+
+		<div id="DivListeTest">
+			<h2>Listes des tests</h2>
+
+			<%@ include file="/formateur/listeTest.jspf"%>
+
+		</div>
+
+		<input type="button" id="btn_add_test" value="Ajouter" /> 
+		<input type="button" id="btn_update_test" value="Modifier" /> 
+		<input type="button" id="btn_remove_test" value="Supprimer" />
+
+		<div id="detailTest">
+			<h2>Détail du test</h2>
+		</div>
+	</div>
+</body>
+</html>
--- ProjetQCM/src/fr/eni_ecole/jee/dal/TestDAO.java
+++ ProjetQCM/src/fr/eni_ecole/jee/dal/TestDAO.java
@@ -3,43 +3,128 @@ package fr.eni_ecole.jee.dal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 
 import fr.eni_ecole.jee.bean.*;
+import fr.eni_ecole.jee.util.AccesBase;
 
 
 public class TestDAO {
 
-	public static void ajouter(Test test) throws Exception{
+	public static void add(Test unTest) throws Exception{
 		Connection cnx=null;
 		PreparedStatement rqt=null;
+		try
+		{
+			cnx = AccesBase.getConnection();
+			rqt = cnx.prepareStatement("INSERT INTO test(libelle, duree, seuil_min,seuil_max) VALUES (?,?,?,?)");
+			rqt.setString(1, unTest.getLibelle());
+			rqt.setInt(2, unTest.getDuree());
+			rqt.setInt(3, unTest.getSeuilMin());
+			rqt.setInt(4, unTest.getSeuilMax());
+			rqt.executeUpdate();
+		}
+		finally
+		{
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
 	}
 
 
-	public static void supprimer(Test test) throws Exception{
+	public static void remove(Test unTest) throws Exception{
+		
 		Connection cnx=null;
 		PreparedStatement rqt=null;
+		try
+		{
+			cnx = AccesBase.getConnection();
+			rqt = cnx.prepareStatement("DELETE FROM test WHERE id = ?");
+			rqt.setInt(1, unTest.getId());
+			rqt.executeUpdate();
+		}
+		finally
+		{
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
 	}
 
-	public static void modifier(Test test) throws Exception{
+	public static void update(Test unTest) throws Exception{
 		Connection cnx=null;
 		PreparedStatement rqt=null;
+		try
+		{
+			cnx = AccesBase.getConnection();
+			rqt = cnx.prepareStatement("UPDATE test SET libelle = ?,duree = ?,seuil_min = ?,seuil_max = ? WHERE id = ?");
+			rqt.setString(1, unTest.getLibelle());
+			rqt.setInt(2, unTest.getDuree());
+			rqt.setInt(3, unTest.getSeuilMin());
+			rqt.setInt(4, unTest.getSeuilMax());
+			rqt.setInt(5, unTest.getId());
+			rqt.executeUpdate();
+		}
+		finally
+		{
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
 	}
 	
-	public static Test rechercher(Test test) throws Exception{
+	public static Test getByID(Test unTest) throws Exception{
 		Connection cnx=null;
 		PreparedStatement rqt=null;
-		ResultSet rs=null;
-		return test;
+		ResultSet rs = null;
+		Test monTest = null;
+		try
+		{
+			cnx = AccesBase.getConnection();
+			rqt = cnx.prepareStatement("SELECT * from test WHERE id= ?");
+			rqt.setInt(1, unTest.getId());
+			rs = rqt.executeQuery();
+			while (rs.next())
+			{
+				monTest = new Test(rs.getInt("id"), rs.getString("libelle"),rs.getInt("duree"),rs.getInt("seuil_min"),rs.getInt("seuil_max"));
+			}
+		}
+		finally
+		{
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+		return monTest;
 	}
 	
-	public static Test rechercherParID(int id)
+	public static ArrayList<Test> getAll() throws SQLException
 	{
 		Connection cnx = null;
-		PreparedStatement rqt = null;
+		Statement rqt = null;
 		ResultSet rs = null;
-		Test monTest = new Test();
+		ArrayList<Test> tests = new ArrayList<Test>();
 		
-		return monTest;
+		try
+		{
+			cnx = AccesBase.getConnection();
+			rqt = cnx.createStatement();			
+			rs = rqt.executeQuery("SELECT * FROM test");
+			Test unTest;
+			while (rs.next())
+			{
+				unTest = new Test(rs.getInt("id"), rs.getString("libelle"),rs.getInt("duree"),rs.getInt("seuil_min"),rs.getInt("seuil_max"));
+				tests.add(unTest);				
+			}
+		}
+		finally
+		{
+			if (rs!=null) rs.close();
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+		
+		return tests;
 		
 	}
 }
