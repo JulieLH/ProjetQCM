7679c840b5c7b5b976898cced2af1117c16a882c
d1410jouennej <d1410jouennej@21-UC36-01.ad.campus-eni.fr>
2015-10-26 09:45:40 +0100

    Ajout GetById dans ThemeDAO

--- ProjetQCM/src/fr/eni_ecole/jee/dal/ThemeDAO.java
+++ ProjetQCM/src/fr/eni_ecole/jee/dal/ThemeDAO.java
@@ -1,10 +1,14 @@
 package fr.eni_ecole.jee.dal;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+import fr.eni_ecole.jee.bean.Section;
+import fr.eni_ecole.jee.bean.Test;
 import fr.eni_ecole.jee.bean.Theme;
 import fr.eni_ecole.jee.util.AccesBase;
 
@@ -39,4 +43,32 @@ public class ThemeDAO
 		
 		return lesThemes;
 	}
+	
+	public static Theme getByID(int idTheme) throws Exception
+	{
+		Connection cnx=null;
+		PreparedStatement rqt=null;
+		ResultSet rs = null;
+		Theme monTheme = null;
+		try
+		{
+			cnx = AccesBase.getConnection();
+			rqt = cnx.prepareStatement("SELECT t.id_theme,t.libelle FROM theme t WHERE t.id_theme = ?");
+			rqt.setInt(1, idTheme);
+			rs = rqt.executeQuery();
+			while (rs.next())
+			{
+				//création d'une section a ajouté dans la liste
+				monTheme = new Theme(rs.getInt("t.id_theme"),rs.getString("t.libelle"));
+			}
+		}
+		finally
+		{
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+		return monTheme;
+	}
+	
+	
 }
